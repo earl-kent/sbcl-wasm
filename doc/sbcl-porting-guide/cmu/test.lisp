0637170043d92660bb3fddb267efdb63700fca8a
Earl Kent <13772486+earl-kent@users.noreply.github.com>
2026-02-27 01:56:25 -0800

    texinfo-ization: tweak correctness

--- doc/sbcl-porting-guide/cmu/test.lisp
+++ doc/sbcl-porting-guide/cmu/test.lisp
@@ -1,8 +1,29 @@
 
 
 
+(flet ((#:cleanup () c))
+  (block #:return
+    (multiple-value-bind
+        (#:next #:start #:count)
+        (block #:unwind
+          (%unwind-protect
+           #'(lambda (x) (return-from #:unwind x)))
+          (%within-cleanup :unwind-protect
+            (return-from #:return p)))
+      (#:cleanup)
+      (%continue-unwind #:next #:start #:count))))
+
+
+
+(block #:foo
+  (%catch #'(lambda () (return-from #:foo (%unknown-values)))
+          'foo)
+  (%within-cleanup :catch
+    @var{xxx}))
+
+
 
 (or (simple-string-p x)
     (and (complex-array-p x)
-	 (= (array-rank x) 1)
-	 (simple-string-p (%array-data x))))
+         (= (array-rank x) 1)
+         (simple-string-p (%array-data x))))
